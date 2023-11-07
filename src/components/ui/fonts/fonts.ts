ccd2838b68976fe6bb68a6c261ff3116dabdc6be
jhean <brizadaojhean@gmail.com>
2023-11-06 21:06:46 -0300

    feat: Fontes do projeto

---
+++ src/components/ui/fonts/fonts.ts
@@ -0,0 +1,13 @@
+import { Cinzel, Open_Sans } from 'next/font/google'
+
+export const cinzel = Cinzel({
+    subsets: ['latin'],
+    weight: ['400'],
+    variable: '--font-cinzel',
+})
+
+export const openSans = Open_Sans({
+    subsets: ['latin'],
+    weight: ['400'],
+    variable: '--font-open-sans',
+})
