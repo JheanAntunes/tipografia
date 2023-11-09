3e8f40e2c3851e26723af2f70a9c7279b45c89bd
jhean <brizadaojhean@gmail.com>
2023-11-09 19:05:21 -0300

    import: Componente Sessão de historia

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import SectionInitial from '@/components/ui/home/main/sectionInitial'
+import Section_History from '@/components/ui/home/main/section_history'
 import Section_O_Que_Tipografia from '@/components/ui/home/main/section_o_que_tipografia'
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
         <main className="my-16 flex flex-col gap-16">
             <SectionInitial />
             <Section_O_Que_Tipografia />
+            <Section_History />
         </main>
     )
 }
