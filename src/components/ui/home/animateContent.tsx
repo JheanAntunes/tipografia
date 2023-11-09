8e9d999a6077d710cbafb9d3f9b1c70ecb5af67a
jhean <brizadaojhean@gmail.com>
2023-11-09 19:00:42 -0300

    feat: um atalho para fazer animações com wrapperAnimationSection

---
+++ src/components/ui/home/animateContent.tsx
@@ -0,0 +1,27 @@
+import WrapperAnimationSection from '@/components/framer/wrapperAnimationSection'
+import { MenuProps } from '@/components/layout/header/nav/menuMobile/MenuConteudo'
+import { cn } from '@/lib/utils'
+
+function AnimateContent({
+    direction = 'left',
+    children,
+    className,
+}: MenuProps) {
+    return (
+        <>
+            <WrapperAnimationSection
+                variant="none"
+                direction={direction}
+                className={cn(
+                    'border-b-4 border-solid border-b-slate-950 pb-5',
+                    direction === 'left' ? 'pr-4' : 'pl-4',
+                    className
+                )}
+            >
+                {children}
+            </WrapperAnimationSection>
+        </>
+    )
+}
+
+export default AnimateContent
