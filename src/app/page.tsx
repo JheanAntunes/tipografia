d02eeee6e506f6848908899b7eed54b977c46159
jhean <brizadaojhean@gmail.com>
2023-11-08 22:55:55 -0300

    docs: arquitetura

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import SectionInitial from '@/components/ui/home/main/sectionInitial'
+import Section_O_Que_Tipografia from '@/components/ui/home/main/section_o_que_tipografia'
 
 export default function Home() {
     return (
         <main className="my-16 flex flex-col gap-16">
             <SectionInitial />
+            <Section_O_Que_Tipografia />
         </main>
     )
 }
--- src/components/ui/layout/header/nav/navSeparador.tsx
+++
@@ -1,7 +0,0 @@
-import { cn } from '@/lib/utils'
-
-function NavSeparador({ className, ...props }: React.ComponentProps<'div'>) {
-    return <div className={cn('w-[1px] bg-slate-950', className)} {...props} />
-}
-
-export default NavSeparador
