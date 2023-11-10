09e2b4b084f8b67c10ea78fe9bc743ce72512f7f
jhean <brizadaojhean@gmail.com>
2023-11-09 21:18:13 -0300

    styles: cn adicionada

--- src/components/ui/home/containerContent.tsx
+++ src/components/ui/home/containerContent.tsx
@@ -1,5 +1,14 @@
-function ContainerContent({ children }: React.PropsWithChildren) {
-    return <div className="grid grid-cols-[1fr_4px_1fr]">{children}</div>
+import { cn } from '@/lib/utils'
+
+function ContainerContent({
+    children,
+    className,
+}: React.ComponentProps<'div'>) {
+    return (
+        <div className={cn('grid grid-cols-[1fr_4px_1fr]', className)}>
+            {children}
+        </div>
+    )
 }
 
 export default ContainerContent
