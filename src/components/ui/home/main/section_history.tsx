9644e462c146a29a9e431a8d44791ef2f23dacdc
jhean <brizadaojhean@gmail.com>
2023-11-11 19:57:50 -0300

    fix: melhorando a leitura na sessão de historia.

--- src/components/ui/home/main/section_history.tsx
+++ src/components/ui/home/main/section_history.tsx
@@ -5,6 +5,7 @@ import QualFonte from '../QualFonte'
 import AnimateContent from '../animateContent'
 import ContainerContent from '../containerContent'
 import LinhaVerticalContent from '../linhaVerticalContent'
+import ContainerParagrafos from './section_Fontes_Tipos/containerParagrafos'
 
 function Section_History() {
     return (
@@ -20,29 +21,48 @@ function Section_History() {
                     direction="left"
                     className="col-span-full @5xl:col-span-1"
                 >
-                    <TypographyP className="max-w-4xl font-openSans leading-normal text-slate-900">
-                        O processo de impressão tipográfico tem sua origem com a
-                        criação dos tipos móveis, realizada pelo alemão
-                        Gutenberg, no início do século XV.Com a invenção, a
-                        imprensa foi rapidamente difundida na Europa, e chegou
-                        na América Latina pelo México, em 1539.No Brasil,
-                        entretanto, essa indústria chegou tarde. Isso porque a
-                        corte portuguesa não permitia a entrada de tipografias
-                        por aqui.Somente em 1808 que o país começou a contar com
-                        esse tipo de impressão. Até então, eram utilizadas
-                        prensas, em que utilizava-se uma matriz e, com ela,
-                        imprimia-se cada folha individualmente.Até 1822, a
-                        Imprensa Régia, com suas prensas de mesa, detinham o
-                        monopólio de impressões brasileiras. Com a independência
-                        do Brasil, iniciou-se o processo de evolução das prensas
-                        e métodos de impressão, com diversos novos modelos.Até
-                        1970, em que o sistema offset roubou a cena por ser mais
-                        adequado às grandes tiragens da indústria gráficas.Com
-                        isso, método de impressão tipográfico perdeu espaço, mas
-                        não foi extinto. Isso porque ele tem características
-                        peculiares, com resultados que outros processos não são
-                        capazes de alcançar.
-                    </TypographyP>
+                    <ContainerParagrafos>
+                        <TypographyP className="max-w-4xl font-openSans leading-normal text-slate-900">
+                            O processo de impressão tipográfico tem sua origem
+                            com a criação dos tipos móveis, realizada pelo
+                            alemão Gutenberg, no início do século XV.
+                        </TypographyP>
+                        <TypographyP>
+                            Com a invenção, a imprensa foi rapidamente difundida
+                            na Europa, e chegou na América Latina pelo México,
+                            em 1539
+                        </TypographyP>
+                        <TypographyP>
+                            .No Brasil, entretanto, essa indústria chegou tarde.
+                            Isso porque a corte portuguesa não permitia a
+                            entrada de tipografias. por aqui.
+                        </TypographyP>
+                        <TypographyP>
+                            Somente em 1808 que o país começou a contar com esse
+                            tipo de impressão. Até então, eram utilizadas
+                            prensas, em que utilizava-se uma matriz e, com ela,
+                            imprimia-se cada folha individualmente. capazes de
+                            alcançar.
+                        </TypographyP>
+                        <TypographyP>
+                            Até 1822, a Imprensa Régia, com suas prensas de
+                            mesa, detinham o monopólio de impressões
+                            brasileiras. Com a independência do Brasil,
+                            iniciou-se o processo de evolução das prensas e
+                            métodos de impressão, com diversos novos modelos.
+                        </TypographyP>
+                        <TypographyP>
+                            Até 1970, em que o sistema offset roubou a cena por
+                            ser mais adequado às grandes tiragens da indústria
+                            gráficas.
+                        </TypographyP>
+                        <TypographyP>
+                            Até Com isso, método de impressão tipográfico perdeu
+                            espaço, mas não foi extinto. Isso porque ele tem
+                            características peculiares, com resultados que
+                            outros processos não são
+                        </TypographyP>
+                    </ContainerParagrafos>
                 </AnimateContent>
                 <LinhaVerticalContent />
                 <AnimateContent
--- src/components/ui/home/main/section_tipografia_moderna.tsx
+++ src/components/ui/home/main/section_tipografia_moderna.tsx
@@ -3,6 +3,7 @@ import QualFonte from '../QualFonte'
 import AnimateContent from '../animateContent'
 import ContainerContent from '../containerContent'
 import LinhaVerticalContent from '../linhaVerticalContent'
+import ContainerParagrafos from './section_Fontes_Tipos/containerParagrafos'
 
 function SectionTipografiaModerna() {
     return (
@@ -15,48 +16,65 @@ function SectionTipografiaModerna() {
                     <QualFonte className="font-ibmSerif">
                         Fonte IBM Sans & IBM Serif
                     </QualFonte>
-                    <TypographyH2 className="font-ibmSerif">
-                        Qual a Importância da{' '}
-                        <span className="@4xl:hidden">Tipografia Moderna?</span>
-                    </TypographyH2>
-                    <TypographyP className="font-ibmSerif">
-                        A indústria tipográfica é, literalmente, o início do
-                        design gráfico e da comunicação imprensa, como um todo.
-                        Por isso, conhecer e respeitar a história é muito
-                        importante para manter viva a alma gráfica.Entender o
-                        passado é uma importante etapa para que fazer o
-                        presente. Conhecer a história pode auxiliar muito na
-                        construção de uma visão crítica {'–'} e ela é essencial
-                        para quem trabalha com design gráfico.Não compreender o
-                        sistema tipográfico acaba sendo uma limitação à
-                        criatividade do designer. Com as novas tecnologias
-                        digitais, ficou ainda muito mais fácil para que o
-                        profissional crie sua própria tipografia e desenvolva
-                        seu alfabeto, criando layouts e marcas únicas,
-                        completamente alinhadas ao interesse e posicionamento da
-                        empresa.
-                    </TypographyP>
+                    <ContainerParagrafos>
+                        <TypographyH2 className="font-ibmSerif">
+                            Qual a Importância da{' '}
+                            <span className="@4xl:hidden">
+                                Tipografia Moderna?
+                            </span>
+                        </TypographyH2>
+                        <TypographyP className="font-ibmSerif">
+                            A indústria tipográfica é, literalmente, o início do
+                            design gráfico e da comunicação imprensa, como um
+                            todo. Por isso, conhecer e respeitar a história é
+                            muito importante para manter viva a alma gráfica.
+                        </TypographyP>
+                        <TypographyP className="font-ibmSerif">
+                            Entender o passado é uma importante etapa para que
+                            fazer o presente. Conhecer a história pode auxiliar
+                            muito na construção de uma visão crítica {'–'} e ela
+                            é essencial para quem trabalha com design gráfico.
+                        </TypographyP>
+                        <TypographyP className="font-ibmSerif">
+                            Não compreender o sistema tipográfico acaba sendo
+                            uma limitação à criatividade do designer.
+                        </TypographyP>
+                        <TypographyP className="font-ibmSerif">
+                            Com as novas tecnologias digitais, ficou ainda muito
+                            mais fácil para que o profissional crie sua própria
+                            tipografia e desenvolva seu alfabeto, criando
+                            layouts e marcas únicas, completamente alinhadas ao
+                            interesse e posicionamento da empresa.
+                        </TypographyP>
+                    </ContainerParagrafos>
                 </AnimateContent>
                 <LinhaVerticalContent />
                 <AnimateContent
                     direction="right"
-                    className="@4xl:mt-[3.32rem] @4xl:flex @4xl:flex-col @4xl:gap-5 @4xl:pl-5"
+                    className="@4xl:mt-[3rem] @4xl:flex @4xl:flex-col @4xl:gap-5 @4xl:pl-5"
                 >
-                    <TypographyH2 className="@4xl:font-ibmSerif hidden @4xl:block">
+                    <TypographyH2 className="hidden @4xl:block @4xl:font-ibmSerif">
                         Tipografia Moderna?
                     </TypographyH2>
-                    <TypographyP className="font-ibmSans mt-1 @4xl:mt-0">
-                        Além disso, é possível alinhar processos de impressão
-                        atuais com o processo tipográfico, para baixas tiragens
-                        e buscando uma apresentação única, rica como uma
-                        gravura.Com o resgate do passado, um certo saudosismo,
-                        cada vez mais forte nas novas gerações, será cada vez
-                        mais comum retomarmos processos, conceitos, ideias e
-                        valores de métodos já ultrapassados.Não à toa, existem
-                        diversos layouts, marcas e mesmo design de ambientes
-                        lançando mão de uma identidade mais retrô, vintage, que
-                        estão na moda hoje em dia.
-                    </TypographyP>
+                    <ContainerParagrafos>
+                        <TypographyP className="mt-1 font-ibmSans @4xl:mt-0">
+                            Além disso, é possível alinhar processos de
+                            impressão atuais com o processo tipográfico, para
+                            baixas tiragens e buscando uma apresentação única,
+                            rica como uma gravura.
+                        </TypographyP>
+                        <TypographyP className="mt-1 font-ibmSans @4xl:mt-0">
+                            Com o resgate do passado, um certo saudosismo, cada
+                            vez mais forte nas novas gerações, será cada vez
+                            mais comum retomarmos processos, conceitos, ideias e
+                            valores de métodos já ultrapassados.
+                        </TypographyP>
+                        <TypographyP className="mt-1 font-ibmSans @4xl:mt-0">
+                            Não à toa, existem diversos layouts, marcas e mesmo
+                            design de ambientes lançando mão de uma identidade
+                            mais retrô, vintage, que estão na moda hoje em dia.
+                        </TypographyP>
+                    </ContainerParagrafos>
                 </AnimateContent>
             </ContainerContent>
         </section>
