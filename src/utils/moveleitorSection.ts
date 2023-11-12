1a8ee537ed8493e12bca325b8e838ab2d5366bcb
jhean <brizadaojhean@gmail.com>
2023-11-12 00:02:41 -0300

    console.log

--- src/utils/moveleitorSection.ts
+++ src/utils/moveleitorSection.ts
@@ -1,5 +1,4 @@
 export const moveLeitorSection = (id: string) => {
     const sectionTop = document.getElementById(id)?.offsetTop
     window.scroll({ behavior: 'smooth', top: sectionTop })
-    console.log('chamando...')
 }
