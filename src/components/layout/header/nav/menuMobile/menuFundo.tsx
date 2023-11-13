d78c9712f019a2f329c0f89ce8d6bd0b196346fb
jhean <brizadaojhean@gmail.com>
2023-11-13 19:56:41 -0300

    styles: dark

--- src/components/layout/header/nav/menuMobile/menuFundo.tsx
+++ src/components/layout/header/nav/menuMobile/menuFundo.tsx
@@ -4,7 +4,10 @@ import { ComponentProps } from 'react'
 function MenuFundo({ children, className, ...props }: ComponentProps<'div'>) {
     return (
         <div
-            className={cn('absolute inset-0 bg-background/80 block', className)}
+            className={cn(
+                'fixed inset-0 z-20 block bg-background/80 dark:bg-slate-900/70',
+                className
+            )}
             {...props}
         >
             {children}
--- src/components/ui/home/progress.tsx
+++ src/components/ui/home/progress.tsx
@@ -1,11 +1,11 @@
 'use client'
 import { MotionStyle, motion } from 'framer-motion'
 
-export default function Progress({ style }: {style:MotionStyle}) {
+export default function Progress({ style }: { style: MotionStyle }) {
     return (
         <>
             <motion.div
-                className="fixed inset-x-0 top-0 h-3 origin-[0%] bg-slate-950"
+                className="fixed inset-x-0 top-0 h-3 origin-[0%] bg-slate-950 dark:bg-slate-50"
                 style={style}
             />
         </>
